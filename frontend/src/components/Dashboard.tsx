e535a126c7a3034ad95fbb5b77fa784674b540db
Santiago Jojoa <santiago.jojoan@gmail.com>
2025-11-15 23:34:28 -0500

    feat: Created default dashboard component and admin dashboard component

---
+++ frontend/src/components/Dashboard.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { AdminDashboard } from './AdminDashboard'
+
+export default function Dashboard() {
+    return (
+        <AdminDashboard />
+    )
+}
+
+//Rednderizar Dashboard sin importar que sea administrador o usuario
