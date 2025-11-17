b4fafe472f24ee528186e0eb1519e35dc5ab8969
Santiago Jojoa <santiago.jojoan@gmail.com>
2025-11-16 23:51:10 -0500

    add: Keyframes and base colors

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -3,7 +3,14 @@ import type { Config } from "tailwindcss";
 const config: Config = {
     darkMode: "class",
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                primary: "#1e90ff",
+                base: "#111a22",
+                card: "#18222d",
+                borderSoft: "#2c3a46",
+            }
+        },
     },
     plugins: [],
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
